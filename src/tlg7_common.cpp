4c8c6dcfa8b781e708d963faebf743ce98bfe7a6
W.Dee <wdee.elec.kit.sup@gmail.com>
2025-09-28 17:46:20 +0900

    feat: refactor prediction functions to simplify parameters and improve clarity

--- src/tlg7_common.cpp
+++ src/tlg7_common.cpp
@@ -121,9 +121,9 @@ namespace tlg::v7
       case 0:
         return 0;
       case 1:
-        return c0;
-      case 2:
         return c0 / 2;
+      case 2:
+        return c0;
       case 3:
         return (3 * c0) / 2;
       default:
@@ -131,18 +131,18 @@ namespace tlg::v7
       }
     }
 
-    inline int predict_secondary(int mode, int c0, int reference1)
+    inline int predict_secondary(int mode, int c0)
     {
       switch (mode & 0x3)
       {
       case 0:
         return 0;
       case 1:
-        return c0;
+        return c0 / 2;
       case 2:
-        return reference1;
+        return c0;
       case 3:
-        return (c0 + reference1) / 2;
+        return (3 * c0) / 2;
       default:
         return 0;
       }
@@ -173,7 +173,7 @@ namespace tlg::v7
       const int d2 = source[perm[2]];
 
       const int predicted1 = predict_primary(params.primary, d0);
-      const int predicted2 = predict_secondary(params.secondary, d0, d1);
+      const int predicted2 = predict_secondary(params.secondary, d0);
 
       const int residual1 = d1 - predicted1;
       const int residual2 = d2 - predicted2;
@@ -204,7 +204,7 @@ namespace tlg::v7
 
       const int restored0 = c0;
       const int restored1 = c1 + predict_primary(params.primary, restored0);
-      const int restored2 = c2 + predict_secondary(params.secondary, restored0, restored1);
+      const int restored2 = c2 + predict_secondary(params.secondary, restored0);
 
       std::array<int, 3> destination = {0, 0, 0};
       destination[perm[0]] = restored0;
