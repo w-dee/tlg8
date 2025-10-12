00b378dd50dac018ba769abb42d46b58c6d0720b
W.Dee <wdee.elec.kit.sup@gmail.com>
2025-10-12 15:03:55 +0900

    Refactor redundant and complex code

--- src/tlg8_interleave.cpp
+++ src/tlg8_interleave.cpp
@@ -1,5 +1,6 @@
 #include "tlg8_interleave.h"
 
+#include <algorithm>
 #include <array>
 
 namespace tlg::v8::enc
@@ -20,6 +21,29 @@ namespace tlg::v8::enc
       }
       return buffer;
     }
+
+    bool should_interleave(InterleaveFilter filter,
+                           uint32_t value_count,
+                           uint32_t used_components)
+    {
+      return filter == InterleaveFilter::Interleave && value_count > 0 && used_components > 1;
+    }
+
+    template <typename IndexProvider>
+    void write_components(component_colors &colors,
+                          const buffer_type &buffer,
+                          uint32_t used_components,
+                          uint32_t value_count,
+                          const IndexProvider &provider)
+    {
+      for (uint32_t comp = 0; comp < used_components; ++comp)
+      {
+        auto &component = colors.values[comp];
+        for (uint32_t i = 0; i < value_count; ++i)
+          component[i] = buffer[provider(comp, i)];
+        std::fill(component.begin() + value_count, component.end(), 0);
+      }
+    }
   }
 
   void apply_interleave_filter(InterleaveFilter filter,
@@ -27,38 +51,22 @@ namespace tlg::v8::enc
                                uint32_t components,
                                uint32_t value_count)
   {
-    if (filter != InterleaveFilter::Interleave)
-      return;
-    if (value_count == 0)
-      return;
-
     const uint32_t used_components = std::min<uint32_t>(components, static_cast<uint32_t>(colors.values.size()));
-    if (used_components <= 1)
+    if (!should_interleave(filter, value_count, used_components))
       return;
 
     const auto source = make_component_buffer(colors, used_components, value_count);
-    std::array<int16_t, kMaxBlockPixels * 4> interleaved{};
-
-    for (uint32_t i = 0; i < value_count; ++i)
-    {
-      for (uint32_t comp = 0; comp < used_components; ++comp)
-      {
-        const uint32_t src_index = comp * value_count + i;
-        const uint32_t dst_index = i * used_components + comp;
-        interleaved[dst_index] = source[src_index];
-      }
-    }
-
-    for (uint32_t comp = 0; comp < used_components; ++comp)
-    {
-      for (uint32_t i = 0; i < value_count; ++i)
-      {
-        const uint32_t index = comp * value_count + i;
-        colors.values[comp][i] = interleaved[index];
-      }
-      for (uint32_t i = value_count; i < kMaxBlockPixels; ++i)
-        colors.values[comp][i] = 0;
-    }
+    write_components(
+        colors,
+        source,
+        used_components,
+        value_count,
+        [used_components, value_count](uint32_t comp, uint32_t i) {
+          const uint32_t index = comp * value_count + i;
+          const uint32_t src_component = index % used_components;
+          const uint32_t src_offset = index / used_components;
+          return src_component * value_count + src_offset;
+        });
   }
 
   void undo_interleave_filter(InterleaveFilter filter,
@@ -66,26 +74,18 @@ namespace tlg::v8::enc
                               uint32_t components,
                               uint32_t value_count)
   {
-    if (filter != InterleaveFilter::Interleave)
-      return;
-    if (value_count == 0)
-      return;
-
     const uint32_t used_components = std::min<uint32_t>(components, static_cast<uint32_t>(colors.values.size()));
-    if (used_components <= 1)
+    if (!should_interleave(filter, value_count, used_components))
       return;
 
-    const auto interleaved = make_component_buffer(colors, used_components, value_count);
-
-    for (uint32_t comp = 0; comp < used_components; ++comp)
-    {
-      for (uint32_t i = 0; i < value_count; ++i)
-      {
-        const uint32_t src_index = i * used_components + comp;
-        colors.values[comp][i] = interleaved[src_index];
-      }
-      for (uint32_t i = value_count; i < kMaxBlockPixels; ++i)
-        colors.values[comp][i] = 0;
-    }
+    const auto source = make_component_buffer(colors, used_components, value_count);
+    write_components(
+        colors,
+        source,
+        used_components,
+        value_count,
+        [used_components](uint32_t comp, uint32_t i) {
+          return i * used_components + comp;
+        });
   }
 }
